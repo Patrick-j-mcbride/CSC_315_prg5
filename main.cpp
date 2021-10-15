c51a6504ba13f83c79cc4b19af4da6301e9e3f1c
Patrick-McBride <patrickjmcbridejr@gmail.com>
2021-10-15 10:52:10 -0600

    added printing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <time.h> 
 #include "QuadraticProbing.h"
+#include <iomanip>
 using namespace std;
+
+struct otp
+{
+    double Qprobe[5];
+    int insers[5];
+    double Lprobe[5];
+};
+
+void printTable(otp ot);
+
 
     // Simple main
 int main(void)
 {
+    otp ot;
     HashTable<int> H;
     int probeCt = 0;
     int numGen = 100;
@@ -15,6 +27,7 @@ int main(void)
 
     for (int k = 0; k < 5; k++)
     {
+        probeCt = 0;
             for( int i = 0; i < numGen; i++)
         {
             for (int j = 0; j < numInsert; j++)
@@ -25,9 +38,34 @@ int main(void)
             H.makeEmpty();
         }
         cout << "Average number of probes for " << numInsert << ":   " << (probeCt/(double)numGen) << endl;
+        ot.insers[k] = numInsert;
+        ot.Qprobe[k] = (probeCt/(double)numGen);
         numInsert += 10;
         numGen = numInsert * 10;
     }
 
+    printTable(ot);
+
     return 0;
+}
+
+void printTable(otp ot)
+{
+    cout << endl << endl << setw(35) << "Quadratic Probing";
+    cout << endl << setw(56) << setfill('_') << "_" << endl;
+    cout << "|";
+    for (int i = 0; i < 5; i++ )
+    {
+        cout << setfill(' ') << setw(10) << left << ((1+i)*10);
+        cout << "|";
+    }
+    cout << endl;
+    cout << setw(56) << setfill('-') << "-" << endl;
+    cout << "|";
+    for (int i = 0; i < 5; i++ )
+    {
+        cout << setfill(' ') << setw(10) << left << (ot.Qprobe[i]);
+        cout << "|";
+    }
+    cout << endl << setw(56) << setfill('_') << '_' << endl << endl << endl;
 }
